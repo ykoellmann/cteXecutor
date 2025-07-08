2b542aa7908953076812acb209f2cd5018b4fe1a
Yannik <yankoe@outlook.de>
2025-07-08 20:32:00 +0200

    feat: add changenotes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,54 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "2.1.21"
-  id("org.jetbrains.intellij.platform") version "2.3.0"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.21"
+    id("org.jetbrains.intellij.platform") version "2.3.0"
 }
 
 group = "com.ykoellmann.ctexecutor"
 version = "1.1.0"
 
 repositories {
-  mavenCentral()
-  intellijPlatform {
-    defaultRepositories()
-  }
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
-  intellijPlatform {
-    create("DB", "2025.1")
-    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    intellijPlatform {
+        create("DB", "2025.1")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
-    // Add necessary plugin dependencies for compilation here, example:
-    bundledPlugin("com.intellij.database")
-  }
+        // Add necessary plugin dependencies for compilation here, example:
+        bundledPlugin("com.intellij.database")
+    }
 }
 
 intellijPlatform {
-  pluginConfiguration {
-    ideaVersion {
-      sinceBuild = "242"
-    }
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+        }
 
-    changeNotes = """
-      Initial version
+        changeNotes = """
+        <ul>
+            <li>Added "Copy and Edit SQL" action, allowing you to append WHERE clauses or other modifications at the end of CTE queries</li>
+            <li>Refactored code structure to reduce duplication</li>
+            <li>Renamed actions for improved clarity and usability</li>
+        </ul>
     """.trimIndent()
-  }
+    }
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "21"
-    targetCompatibility = "21"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "21"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "21"
+    }
 }
